81e6d935c088cb06f6886d205449bb0556e093e5
Dmitriy <dimokse@gmail.com>
2023-08-15 18:03:11 +0300

    Add скачивание картинок и добавлени их в docx

--- my_main.py
+++ my_main.py
@@ -1,22 +1,43 @@
-import hashlib
 import os
+import hashlib
 import requests
 from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.oxml.ns import qn
 from io import BytesIO
-
-# URL, где хранится JSON данные
-json_url = "https://academyopen.ru/api-7/news/903"
+import html2text
+from docx.shared import Inches
 
 # Список URL для JSON данных
 json_urls = [
     "https://academyopen.ru/api-7/news/903",
     "https://academyopen.ru/api-7/news/904",
-
-
+    # ... добавьте другие URL-ы с JSON данными
 ]
+
+
+def add_empty_line(doc):
+    doc.add_paragraph("")
+
+
+def html_to_plain_text(html):
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    return h.handle(html)
+
+
+def add_text_block(doc, text, font_size, font_style=None, alignment=WD_PARAGRAPH_ALIGNMENT.LEFT,
+                   spacing_after=Pt(0)):
+    paragraph = doc.add_paragraph(text)
+    paragraph.alignment = alignment
+    paragraph.paragraph_format.space_after = spacing_after
+    run = paragraph.runs[0]
+    run.font.size = Pt(font_size)
+    if font_style == 'bold':
+        run.font.bold = True
+    elif font_style == 'italic':
+        run.font.italic = True
+    return paragraph
 
 
 # Для каждого URL получаем JSON данные и создаем документ
@@ -24,121 +45,86 @@ for json_url in json_urls:
     response = requests.get(json_url)
     your_json = response.json()
 
-
     # Создаем папку для сохранения документов, если её еще нет
     title = your_json["data"]["title"]
-    title_for_folder = title.replace('/', '_').replace(':', '_')    # Если в название статьи недопустимые символы
+    title_for_folder = title.replace('/', '_').replace(':', '_')
     folder_path = os.path.join(os.getcwd(), title_for_folder)
-
-    # Создание папки
     os.makedirs(folder_path, exist_ok=True)
     print(f"Создана директория: {folder_path}")
 
     # Путь к файлу .docx внутри папки
     docx_filename = os.path.join(folder_path, f"{title_for_folder}.docx")
-    print(f"Создан файл: {docx_filename}\n+---------------------------------------------------------------------------------+")
+    print(f"Создан файл: {docx_filename}")
 
-    # Создание счетчика для скачанных фото
-    downloaded_images_count = 0
+    # Создание документа
+    doc = Document()
+    doc.styles['Normal'].font.name = 'Times New Roman'
 
-    # Увеличение счетчика скаченных изображений
-    downloaded_images_count += 1
+    # Добавление названия статьи
+    add_text_block(doc, title, 14, alignment=WD_PARAGRAPH_ALIGNMENT.CENTER)
+    print(f"Title: {title}")
 
-    # Скачивание и сохранение изображения в папку с названием статьи
-    image_url = your_json["data"]["socialPhoto"]
-    image_response = requests.get(image_url)
-    image_path = os.path.join(folder_path, "image.jpg")  # Задайте имя файла изображения
-    with open(image_path, "wb") as image_file:
-        image_file.write(image_response.content)
-    print(f"Изображение сохранено: {image_path}")
+    # Получение значения из поля "subtitle"
+    subtitle = your_json["data"]["subtitle"]
+    print(f"Subtitle: {subtitle}")
 
-    # Скачивание и сохранение изображений из блока "carousel"
+    add_text_block(doc, subtitle, 12, alignment=WD_PARAGRAPH_ALIGNMENT.CENTER)  # Подзаголовок
+    add_empty_line(doc)  # Вызов функции для добавления пустой строки
+
+    # Получение значения "materials"
+    materials = your_json["data"]["materials"]
+    print(f"Доп материалы: {materials}")
+
+    # Добавление текстовых блоков из "blocks"
+    for block in your_json["data"]["blocks"]:
+        block_type = block["blockType"]
+
+        if block_type == 1:
+            text = block.get("text")
+            if text:
+                add_text_block(doc, text, 10, alignment=WD_PARAGRAPH_ALIGNMENT.LEFT)
+                add_empty_line(doc)
+        elif block_type == 10:
+            text = block.get("text")
+            if text:
+                add_text_block(doc, text, 16, alignment=WD_PARAGRAPH_ALIGNMENT.CENTER)
+                add_empty_line(doc)
+        elif block_type == 11:  # Список
+            elem_list = block.get("elemList")
+            if elem_list and elem_list["elemType"] == 2:
+                elems = elem_list["elems"]
+                if elems:
+                    for elem in elems:
+                        add_text_block(doc, "- " + elem, 10, alignment=WD_PARAGRAPH_ALIGNMENT.LEFT)
+                    add_empty_line(doc)
+
+    # Скачивание и сохранение изображений из блока "blockType": 5 "carousel"
+
     for block in your_json["data"]["blocks"]:
         if block["blockType"] == 5 and "carousel" in block:
             carousel_images = block["carousel"]
             for index, carousel_item in enumerate(carousel_images):
                 image_url = carousel_item["image"]
                 image_response = requests.get(image_url)
-                image_extension = image_url.split(".")[-1]  # Получение расширения изображения
-                image_hash = hashlib.md5(
-                    image_response.content).hexdigest()  # Генерация хэша из содержимого изображения
+                image_extension = image_url.split(".")[-1]
+                image_hash = hashlib.md5(image_response.content).hexdigest()
                 image_filename = f"carousel_{image_hash}.{image_extension}"
                 image_path = os.path.join(folder_path, image_filename)
                 with open(image_path, "wb") as image_file:
                     image_file.write(image_response.content)
-                downloaded_images_count += 1
                 print(f"Изображение из блока 'carousel' сохранено: {image_path}")
-        # Вывод числа скаченных фото для текущей статьи
-    print(f"Для статьи '{title}' скачано {downloaded_images_count} изображений")
-    # Создание документа
-    doc = Document()
-    doc.styles['Normal'].font.name = 'Times New Roman'
 
+                # Вставка изображения в docx
+                doc.add_picture(image_path, width=Inches(6.0))  # Изменение размеров картинки
 
-    # Функция для добавления текстового блока с заданными параметрами
-    def add_text_block(text, font_size, alignment=WD_PARAGRAPH_ALIGNMENT.LEFT, spacing_after=Pt(0)):
-        paragraph = doc.add_paragraph(text)
-        paragraph.runs[0].font.size = Pt(font_size)
-        paragraph.alignment = alignment
-        paragraph.paragraph_format.space_after = spacing_after
-        return paragraph
+                # Добавление подписи к изображению
+                sign = carousel_item.get("sign")
+                if sign:
+                    add_empty_line(doc)
+                    add_text_block(doc, "Комментарий:", 12, WD_PARAGRAPH_ALIGNMENT.LEFT)
+                    add_text_block(doc, sign, 10, font_style='italic', alignment=WD_PARAGRAPH_ALIGNMENT.LEFT)
+                    add_empty_line(doc)
 
-
-    # Функция для добавления пустой строки
-    def add_empty_line():
-        doc.add_paragraph("")
-
-
-    # # Добавление названия рубрики
-    # rubric_name = your_json["data"]["rubric"]["name"]
-    # add_text_block(rubric_name, 10, WD_PARAGRAPH_ALIGNMENT.CENTER)
-
-    # Добавление названия статьи
-    title = your_json["data"]["title"]
-    add_text_block(title, 14, WD_PARAGRAPH_ALIGNMENT.CENTER)
-    add_empty_line()
-
-    # Добавление СЕО описания
-    # seo_title = your_json["data"]["rubric"]["seoTitle"]
-    # add_text_block(seo_title, 10, WD_PARAGRAPH_ALIGNMENT.CENTER)
-    # add_empty_line()
-    # seo_description = your_json["data"]["rubric"]["seoDescription"]
-    # add_text_block(seo_description, 10, WD_PARAGRAPH_ALIGNMENT.LEFT)
-    # add_empty_line()
-
-    # Добавление описания SEO
-    seo_description = your_json["data"]["rubric"]["seoDescription"]
-
-    # Добавляем пометку перед следующей строкой
-    doc.add_paragraph("seoDescription", style='Normal')
-    doc.add_paragraph(seo_description, style='Normal')  # Добавляем описание SEO
-
-    # Добавление заголовка
-    subtitle = your_json["data"]["subtitle"]
-    add_text_block(subtitle, 16, WD_PARAGRAPH_ALIGNMENT.CENTER)
-    add_empty_line()
-
-    # Добавление подзаголовка
-    date = your_json["data"]["date"]
-    add_text_block(date, 12, WD_PARAGRAPH_ALIGNMENT.CENTER)
-    add_empty_line()
-
-    # Добавление ссылки на фото
-
-    social_photo_url = your_json["data"]["socialPhoto"]
-    response = requests.get(social_photo_url)
-    image_stream = BytesIO(response.content)
-    doc.add_picture(image_stream)
-
-    # Добавление текстовых блоков из "blocks"
-    for block in your_json["data"]["blocks"]:
-        if block["blockType"] == 1:
-            text = block["text"]
-            add_text_block(text, 10)
-            add_empty_line()
-
-    # Сохранение документа в папку с названием статьи
-    if not os.path.exists(title):
-        os.makedirs(title)
-    doc.save(os.path.join(title, docx_filename))
-
+    # Сохранение документа названием статьи в папку с названием статьи
+    doc.save(docx_filename)
+    print(f"Документ сохранен: {docx_filename}\n------------------------------------------------------------------------------")
